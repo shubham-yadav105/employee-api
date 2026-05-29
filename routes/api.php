9fb1e6172e040a1073a69c85c9a78dab25662a43
shubham Yadav <sy907219@gmail.com>
2026-05-29 15:41:12 +0530

    new auth routes Added in api.php

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\EmployeeController;
+use App\Http\Controllers\Api\AuthController;
+
 
 Route::apiResource('employees', EmployeeController::class);
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
